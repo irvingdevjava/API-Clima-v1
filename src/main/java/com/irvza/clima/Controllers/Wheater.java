8fca1380f18c43ffd5e6cb9f7c593f29cf97571a
irvingdevjava <irvingspringboot@gmail.com>
2024-07-22 22:14:55 -0600

    Update

--- src/main/java/com/irvza/clima/Controllers/Wheater.java
+++ src/main/java/com/irvza/clima/Controllers/Wheater.java
@@ -3,11 +3,10 @@ package com.irvza.clima.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.irvza.clima.Service.CallApi;
 
 
@@ -19,9 +18,8 @@ public class Wheater {
     CallApi callApi;
 
     @GetMapping("/weather")
-    public ResponseEntity<JsonNode> getMethodName(@RequestBody String country) {
-        JsonNode response=callApi.getInfo(country);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<?> getMethodName(@RequestParam String location) {
+        return ResponseEntity.ok(callApi.getInfo(location));
     }
     
 
--- src/main/java/com/irvza/clima/Service/CallApi.java
+++ src/main/java/com/irvza/clima/Service/CallApi.java
@@ -3,25 +3,26 @@ package com.irvza.clima.Service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.irvza.clima.Configuracion.UTF8RestTemplate;
+import com.irvza.clima.Modelos.WeatherResponse;
 
 @Service
 public class CallApi {
-
-    public JsonNode getInfo(String country) {
+    private final String apiKey = "";
+    private final String weatherUrl = "http://api.weatherapi.com/v1/current.json?key={apiKey}&q={location}&lang=es";
+    public WeatherResponse getInfo(String location) {
+       
         try {
-            RestTemplate restTemplate = new RestTemplate();
-            String url = "http://api.weatherapi.com/v1/current.json?key=&q=" + country + "&aqi=no&lang=es";
-            String response = restTemplate.getForObject(url, String.class);
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readTree(response);
+            RestTemplate restTemplate = new UTF8RestTemplate().createRestTemplate();
+            return restTemplate.getForObject(weatherUrl, WeatherResponse.class, apiKey, location);
         } catch (Exception e) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.createObjectNode().put("error", "Error al obtener información del clima. " + e.getMessage());
-        }}
+            System.out.println("Error al obtener información del clima. " + e.getMessage());
+            return null;
+        }
+    }
 
 }
+
 
 
 
